17722d415f0715ca8bf3414dfb17c7a205da8974
kilicmu <kilicmu3389@outlook.com>
2021-03-19 23:43:02 +0800

    fix: remove console

--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@
 function render(template, data) {
   let head = "let str = '';with(obj){str+=`",
     tail = "`; return str;}";
-  const variable = /\{\{([^}]+)\}\}/g; // => 获取 {{ var }} 语法 =>提取 (var)
-  const expression = /\{\%([^%]+)\%\}/g; // => 获取 {{% expression %}} 表达式 =>提取 (expression)
+  const variable = /\{\{([^}]+)\}\}/g; // => 获取 {{ var }} 语法 => 提取 (var)
+  const expression = /\{\%([^%]+)\%\}/g; // => 获取 {{% expression %}} 表达式 => 提取 (expression)
   template = head + template;
   template = template.replace(variable, (_, r) => "${" + r + "}");
-  template = template.replace(expression, (_, r) => {
-    return "`;" + r + ";str+=`";
-  });
+  template = template.replace(expression, (_, r) => "`;" + r + ";str+=`");
   template += tail;
-  console.log(template)
   const fn = new Function("obj", template);
   return fn(data).replace('\r\n', '');
 }
